5009c2813fa8a520d08724ce9e2212f8c89fec58
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-01-22 02:29:19 +1100

    Updated incorrect foreign key for Product

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
